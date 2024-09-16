64fd17b2de854dfd1cdf4dc2a22f8fd39f5fd7fe
cauan <cauandacruzferreira1010@gmail.com>
2024-09-16 13:11:37 -0300

    app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Inicio from './paginas/Inicio';
+import Servicos from './paginas/servicos';
+import HistoricoServicos from './paginas/HistoricoServicos';
+import NovoVeiculo from './paginas/NovoVeiculo';
+import VeiculosSalvos from './paginas/VeiculoSalvo';
+import BarraLateral from './Components/BarraLateral';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+
+      <div className="app-container">
+        <BarraLateral />
+        
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/servicos" element={<Servicos />} />
+          <Route path="/historico" element={<HistoricoServicos />} />
+          <Route path="/novoveiculo" element={<NovoVeiculo />} />
+          <Route path="/veiculossalvos" element={<VeiculosSalvos />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
